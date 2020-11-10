2d2713d26c315bc9b8780861362ad637eae6128f
jay shah <jaypshah99@gmail.com>
2020-11-10 17:24:06 +0530

    made proper crud apis for guest-service and connected to the data base.

--- guest-service/src/main/java/com/springcloud/guestservice/resource/model/Guest.java
+++ guest-service/src/main/java/com/springcloud/guestservice/resource/model/Guest.java
@@ -1,14 +1,21 @@
-package com.springcloud.guestservice;
+package com.springcloud.guestservice.resource.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Guest {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private String address;
