92f0e61ea7bd8adffdd3b47a0cda450cd15b8850
jay shah <jaypshah99@gmail.com>
2020-11-12 14:59:43 +0530

    Added Respose class for Reservation table.

---
+++ reservation-service/src/main/java/com/springcloud/reservationservice/resource/pojo/Response.java
@@ -0,0 +1,18 @@
+package com.springcloud.reservationservice.resource.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+
+    @JsonProperty("error")
+    private boolean isError;
+
+    @JsonProperty("message")
+    private String message;
+}
