9e0cb2f25e0240ee8f8a065d53fa238f5cb2d14f
jay shah <jaypshah99@gmail.com>
2020-11-17 19:53:28 +0530

    made room service eureka client

--- reservation-service/src/main/java/com/springcloud/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/springcloud/reservationservice/ReservationServiceApplication.java
@@ -2,8 +2,10 @@ package com.springcloud.reservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
